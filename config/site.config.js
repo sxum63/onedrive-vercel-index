73df0291f924d4f6d4a2a4ede33b6c196ae7174e
sxum63 <112649571+sxum63@users.noreply.github.com>
2022-09-28 21:03:51 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,8 +35,7 @@ module.exports = {
   // googleFontLinks -  an array of links for referencing the google font assets.
   googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
 
-  // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
-  // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
+
   
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
